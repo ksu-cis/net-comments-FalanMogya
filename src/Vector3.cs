276ccebcb8855e35ee4f79fdcf84134220d5a9c6
George Widenor <eaglewidenor@users.campus>
2019-09-16 14:21:49 -0500

    Added Crossproduct and learned about xml comments

--- src/Vector3.cs
+++ src/Vector3.cs
@@ -37,15 +37,40 @@ namespace VectorMath
         /// <summary>
         /// Constructs a new instance of Vector3 with
         /// the supplied values for X, Y, and Z.
-        /// <param name="X">A double precision number for the x component of the vector.</param>
-        /// <param name="Y">A double precision number for the y component of the vector.</param>
-        /// <param name="Z">A double precision number for the z component of the vector.</param>
         /// </summary>
+        /// <param name="x">A double precision number for the x component of the vector.</param>
+        /// <param name="y">A double precision number for the y component of the vector.</param>
+        /// <param name="z">A double precision number for the z component of the vector.</param>
         public Vector3(double x, double y, double z)
         {
             this.X = x;
             this.Y = y;
             this.Z = z;
+        }
+
+        /// <summary>
+        /// Gets the magnitude of the vector
+        /// </summary>
+        public double Magnitude
+        {
+            get
+            {
+                return Math.Sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
+            }
+        }
+
+        /// <summary>
+        /// Calculates te cross product of this and the oter vector (Maybe?)
+        /// </summary>
+        /// <param name="other">The vector to cross with this one</param>
+        /// <returns>The cross product</returns>
+        public Vector3 CrossProduct(Vector3 other)
+        {
+            return new Vector3(
+                this.Y * other.Z - this.Z * other.Y,
+                this.Z * other.X - this.X * other.Z,
+                this.X * other.Y - this.Y * other.X
+                );
         }
     }
 }
